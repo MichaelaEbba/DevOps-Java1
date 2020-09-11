d9dad67543f11ba582dac3295e536aafd3e02056
MichaelaEbba <70271597+MichaelaEbba@users.noreply.github.com>
2020-09-11 16:03:07 +0200

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,30 +2,38 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args ) {
         boolean isRunning = true;
+
         Scanner in = new Scanner(System.in);
 
         while (isRunning) {
+            System.out.println("Choose operator");
             String operator = in.next();
-            switch(operator) {
+
+             try {
+                 System.out.println("Please, enter a number");
+                 
+             } catch (NumberFormatException ex) {
+                 
+
+             }
+        
+             System.out.println("Num1:");
+             double num1 = in.nextDouble();
+             System.out.println("Num2:");
+             double num2 = in.nextDouble();
+
+             switch(operator) { //löser så att du kan byta mellan +-*/
                 case "+":
-                    double num1 = in.nextDouble();
-                    double num2 = in.nextDouble();
                     System.out.println(addition(num1, num2));
                     break;
                 case "-":
-                    double num3 = in.nextDouble();
-                    double num4 = in.nextDouble();
-                    System.out.println(subtraction(num3, num4));
+                     System.out.println(subtraction(num1, num2));
                     break;
                 case "*":
-                    double num5 = in.nextDouble();
-                    double num6 = in.nextDouble();
-                    System.out.println(multiplication(num5, num6));
+                    System.out.println(multiplication(num1, num2));
                     break;
                 case "/":
-                    double num7 = in.nextDouble();
-                    double num8 = in.nextDouble();
-                    System.out.println(division(num7, num8));}
+                    System.out.println(division(num1, num2));}
                     
         
 
@@ -45,18 +53,18 @@ public class Calculator {
         return result;
     }
 
-    public static double subtraction(double num3, double num4) {
-        double result = num3 - num4;
+    public static double subtraction(double num1, double num2) {
+        double result = num1 - num2;
         return result;
     }
 
-    public static double multiplication(double num5, double num6) {
-        double result = num5 * num6;
+    public static double multiplication(double num1, double num2) {
+        double result = num1 * num2;
         return result;
     }
 
-    public static double division(double num7, double num8) {
-        double result = num7 / num8;
+    public static double division(double num1, double num2) {
+        double result = num1 / num2;
         return result;
     }
 }
